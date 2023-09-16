06c182557e0503aecd040fae21980611071c82e6
petrosam22 <petrosam22>
2023-09-17 00:31:14 +0300

    change-route

--- app/Http/Controllers/api/GameController.php
+++ app/Http/Controllers/api/GameController.php
@@ -8,11 +8,13 @@ use Illuminate\Http\Request;
 class GameController extends Controller
 {
 
-    public function getGameTurns(Request $request)
+    // public function getGameTurns(Request $request)
+    public function getGameTurns(Request $request, $players = 3, $turns = 3)
+
     {
         $playersCount = $request->get('players', 3);
         $turnsCount = $request->get('turns', 3);
-        $startPlayerIndex = $request->get('start', 1) - 1;
+        $startPlayerIndex = $request->get('start', 2) - 1;
 
         $players = range('A', 'Z');
         $players = array_slice($players, 0, $playersCount);
@@ -27,3 +29,7 @@ class GameController extends Controller
     }
 
 }
+
+
+
+
